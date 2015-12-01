d9478c07a754f2ed536411d11425d057ac433487
Octogonapus <benasur@charter.net>
2015-11-30 22:28:20 -0500

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,263 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Ryan Benasutti on 11/30/2015.
+ */
+
+public class Main
+{
+    //A list of all possible cell values
+    public static final Integer fullPossibilityList[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> possibilities = new ArrayList<>();
+        Collections.addAll(possibilities, fullPossibilityList);
+
+        Puzzle puzzle = new Puzzle();
+        System.out.println("\n" + puzzle.toString());
+
+        //puzzle = solvePuzzle(puzzle);
+        //System.out.println(puzzle.toString());
+
+        //System.out.println(getPeers(puzzle, 0, 0).toString());
+        System.out.println(symmetricDifference(getPeers(puzzle, 6, 8), possibilities));
+    }
+
+    public static Puzzle solvePuzzle(Puzzle puzzle)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (puzzle.data[i][j] == 0)
+                    puzzle = solveCell(puzzle, i, j);
+            }
+        }
+
+        return puzzle;
+    }
+
+    public static Puzzle solveCell(Puzzle puzzle, int i, int j)
+    {
+        ArrayList<Integer> possibilities = new ArrayList<>();
+        Collections.addAll(possibilities, fullPossibilityList);
+
+
+
+        return puzzle;
+    }
+
+    /**
+     * Returns the peers of a cell (duplicate peers are removed)
+     *
+     * @param puzzle The puzzle to get peers from
+     * @param i      The x coordinate of the cell
+     * @param j      The y coordinate of the cell
+     * @return       An ArrayList of the values of the peers of the cell at (x, y) in puzzle
+     */
+    public static ArrayList<Integer> getPeers(Puzzle puzzle, int i, int j)
+    {
+        ArrayList<Integer> peers = new ArrayList<>();
+
+        //Add peers in the same row
+        for (int x = 0; x < 9; x++)
+        {
+            if (x != i)
+            {
+                System.out.print(puzzle.data[j][x]);
+                peers.add(puzzle.data[j][x]);
+            }
+            else
+                System.out.print("x");
+        }
+
+        System.out.print(",");
+
+        //Add peers in the same column
+        for (int y = 0; y < 9; y++)
+        {
+            if (y != j)
+            {
+                System.out.print(puzzle.data[y][i]);
+                peers.add(puzzle.data[y][i]);
+            }
+            else
+                System.out.print("y");
+        }
+
+        System.out.print("\n");
+
+        //Add peers in same section
+        //Left section
+        if (i <= 2)
+        {
+            //Top section
+            if (j <= 2)
+            {
+                for (int k = 0; k <= 2; k++)
+                {
+                    for (int l = 0; l <= 2; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+            //Bottom section
+            else if (j >= 6)
+            {
+                for (int k = 0; k <= 2; k++)
+                {
+                    for (int l = 6; l <= 8; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+            //Middle section
+            else
+            {
+                for (int k = 0; k <= 2; k++)
+                {
+                    for (int l = 3; l <= 5; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+        }
+        //Right section
+        else if (i >= 6)
+        {
+            //Top section
+            if (j <= 2)
+            {
+                for (int k = 6; k <= 8; k++)
+                {
+                    for (int l = 0; l <= 2; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+            //Bottom section
+            else if (j >= 6)
+            {
+                for (int k = 6; k <= 8; k++)
+                {
+                    for (int l = 6; l <= 8; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+            //Middle section
+            else
+            {
+                for (int k = 6; k <= 8; k++)
+                {
+                    for (int l = 3; l <= 5; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+        }
+        //Middle section
+        else
+        {
+            //Top section
+            if (j <= 2)
+            {
+                for (int k = 3; k <= 5; k++)
+                {
+                    for (int l = 0; l <= 2; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+            //Bottom section
+            else if (j >= 6)
+            {
+                for (int k = 3; k <= 5; k++)
+                {
+                    for (int l = 6; l <= 8; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+            //Middle section
+            else
+            {
+                for (int k = 3; k <= 5; k++)
+                {
+                    for (int l = 3; l <= 5; l++)
+                    {
+                        if (k == i && l == j)
+                            assert true;
+                        else
+                            peers.add(puzzle.data[l][k]);
+                    }
+                }
+            }
+        }
+
+        //Remove duplicates
+        Set<Integer> tempSet = new HashSet<>();
+        tempSet.addAll(peers);
+        peers.clear();
+        peers.addAll(tempSet);
+
+        return peers;
+    }
+
+    /**
+     * Returns the symmetric difference of two sets
+     *
+     * @param a Set a
+     * @param b Set b
+     * @return  The symmetric difference of sets a and b
+     */
+    public static ArrayList<Integer> symmetricDifference(ArrayList<Integer> a, ArrayList<Integer> b)
+    {
+        Set<Integer> result = new HashSet<>(a);
+
+        for (Integer element : b)
+            if (!result.add(element))
+                result.remove(element);
+
+        ArrayList<Integer> realResult = new ArrayList<>();
+        realResult.addAll(result);
+
+        return realResult;
+    }
+}
---
+++ src/Puzzle.java
@@ -0,0 +1,85 @@
+/**
+ * Created by Ryan Benasutti on 11/30/2015.
+ *
+ * A representation of a 9x9 sudoku puzzle
+ */
+
+
+public class Puzzle
+{
+    //Puzzle data
+    public int data[][] = new int[9][9];
+
+    public Puzzle()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                data[i][j] = 0;
+            }
+        }
+
+        data[0][0] = 9;
+        data[0][1] = 5;
+        data[0][5] = 4;
+        data[0][7] = 8;
+
+        data[1][2] = 4;
+        data[1][7] = 9;
+        data[1][8] = 5;
+
+        data[2][0] = 8;
+        data[2][1] = 2;
+        data[2][4] = 1;
+        data[2][8] = 3;
+
+        data[3][0] = 4;
+        data[3][3] = 5;
+        data[3][7] = 3;
+        data[3][8] = 8;
+
+        data[4][0] = 3;
+        data[4][1] = 9;
+        data[4][3] = 8;
+        data[4][5] = 1;
+        data[4][7] = 5;
+        data[4][8] = 2;
+
+        data[5][0] = 5;
+        data[5][1] = 8;
+        data[5][5] = 9;
+        data[5][8] = 7;
+
+        data[6][0] = 6;
+        data[6][4] = 4;
+        data[6][7] = 2;
+        data[6][8] = 9;
+
+        data[7][0] = 1;
+        data[7][1] = 4;
+        data[7][6] = 5;
+
+        data[8][1] = 3;
+        data[8][3] = 6;
+        data[8][7] = 1;
+        data[8][8] = 4;
+    }
+
+    @Override
+    public String toString()
+    {
+        String string = "";
+
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                string += j == 2 || j == 5 ? data[i][j] + "|" : data[i][j];
+            }
+            string += i == 2 || i == 5 ? "\n---+---+---\n" : "\n";
+        }
+
+        return string;
+    }
+}
